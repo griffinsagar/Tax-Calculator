8b31e3f2d5677bd35c80363a39fda6f8612c997e
Griffin Sagar <gsagar@uvm.edu>
2020-09-15 11:43:33 -0400

    Added private functions to calculate tax rates

--- TaxVT.cpp
+++ TaxVT.cpp
@@ -1,15 +1,18 @@
-#include "Tax.h"
+#include "TaxVT.h"
 
-Tax::Tax(int income) {
+TaxVT::TaxVT(double income, filingStatus filing) {
     this->income = income;
+    this->filing = filing;
     this->taxOwed = getTaxes();
     this->taxRate = taxOwed / income;
 }
 
+TaxVT::TaxVT() : TaxVT(0, single) {}
+
 // Filing singly
-double Tax::getTaxes() {
+double TaxVT::scheduleX() const {
     if (income > 200200) {
-        return 12968 + ((income - 200200) * 8.75)
+        return 12968 + ((income - 200200) * 8.75);
     } else if (income > 96000) {
         return 5049 + ((income - 39600) * 7.60);
     } else if (income > 75000) {
@@ -20,3 +23,12 @@ double Tax::getTaxes() {
         return income * 3.35;
     }
 }
+
+// Filing separate, married
+double TaxVT::scheduleY1() const {}
+
+// Filing together, married
+double TaxVT::scheduleY2() const {}
+
+// Filing as head of household
+double TaxVT::scheduleZ() const {}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include "TaxVT.cpp"
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TaxVT griffin(10000);
+
+    cout << "2019 Taxes owed: " << griffin.getTaxes();
+
     return 0;
 }
