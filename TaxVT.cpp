e08427ae56c62ae649b49505d639236b42b5a153
Griffin Sagar <gsagar@uvm.edu>
2020-09-15 13:07:11 -0400

    Finished tax bracket functions, getTaxes function

--- TaxVT.cpp
+++ TaxVT.cpp
@@ -24,8 +24,23 @@ double TaxVT::scheduleX(double income) {
     }
 }
 
-// Filing separate, married
+// Filing jointly, married
 double TaxVT::scheduleY1(double income) {
+    if (income > 243750) {
+        return 14776 + ((income - 243750) * 0.0875);
+    } else if (income > 159950) {
+        return 8407 + ((income - 159950) * 0.0760);
+    } else if (income > 75000) {
+        return 2800 + ((income - 75000) * 0.0660);
+    } else if (income > 66150) {
+        return 2216 + ((income - 66150) * 0.0660);
+    } else {
+        return income * 0.0335;
+    }
+}
+
+// Filing separate, married
+double TaxVT::scheduleY2(double income) {
     if (income > 121875) {
         return 7388 + ((income - 121875) * 0.0875);
     } else if (income > 79975) {
@@ -39,21 +54,34 @@ double TaxVT::scheduleY1(double income) {
     }
 }
 
-// Filing together, married
-double TaxVT::scheduleY2(double income) {
-    return 0;
-}
-
 // Filing as head of household
 double TaxVT::scheduleZ(double income) {
-    return 0;
+    if (income > 221950) {
+        return 13372 + ((income - 221950) * 0.0875);
+    } else if (income > 137050) {
+        return 7320 + ((income - 13750) * 0.0760);
+    } else if (income > 75000) {
+        return 3224 + ((income - 75000) * 0.0660);
+    } else if (income > 53100) {
+        return 1779 + ((income - 53100) * 0.0660);
+    } else {
+        return income * 0.0035;
+    }
 }
 
 // Get Taxes
-double TaxVT::getTaxes() {
-   if (filing == single) {
-       return scheduleX(income);
+double TaxVT::getTaxes() const {
+   switch (filing) {
+       case single:
+           return scheduleX(income);
+       case marriedJoint:
+           return scheduleY1(income);
+       case marriedSeparate:
+           return scheduleY2(income);
+       case headOfHouse:
+           return scheduleZ(income);
+       default:
+           return 0;
    }
-   return 0;
 }
 
