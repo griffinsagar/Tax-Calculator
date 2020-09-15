ee381cd03c19e1938ce37e7910da291a9a23716e
Griffin Sagar <gsagar@uvm.edu>
2020-09-15 16:34:11 -0400

    Finished user prompts

--- main.cpp
+++ main.cpp
@@ -1,14 +1,56 @@
 #include <iostream>
+#include <locale>
 #include "TaxVT.h"
 
 using namespace std;
 
+template<class T>
+bool getUserInput(T& variable) {
+    while(!(cin >> variable)) {
+        cout << "Invalid input.";
+        cin.clear();
+        string junk;
+        getline(cin, junk);
+    }
+    cout << endl;
+}
+
 int main() {
-    TaxVT griffin(75000, single);
+    bool running = true;
 
-    cout << "2019 Taxes owed: " << griffin.getTaxes() << endl;
+    while (running) {
+        double usrIncome;
+        int usrStatus;
+        cout << "Please enter your 2019 income: $";
+        getUserInput(usrIncome);
 
-    cout << "Effective tax rate: " << griffin.getTaxRate() << endl;
+        cout << "Filing status options" << endl
+             << "   1. Single" << endl
+             << "   2. Married, joint" << endl
+             << "   3. Married, separate" << endl
+             << "   4. Head of Household" << endl
+             << "Please enter your filing status: ";
+        getUserInput(usrStatus);
+
+        TaxVT user;
+        switch (usrStatus) {
+            case 1:
+                user = TaxVT(usrIncome, single);
+                break;
+            case 2:
+                user = TaxVT(usrIncome, marriedJoint);
+                break;
+            case 3:
+                user = TaxVT(usrIncome, marriedSeparate);
+                break;
+            case 4:
+                user = TaxVT(usrIncome, headOfHouse);
+                break;
+            default:
+                running = false;
+        }
+
+    }
 
     return 0;
 }
