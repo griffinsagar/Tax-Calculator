b50f7ea55066b844c02088553f0d1a14d15f5f66
Griffin Sagar <gsagar@uvm.edu>
2020-09-15 12:47:11 -0400

    Added Schedule Y-1, restrucutred functions to be public static members

--- TaxVT.cpp
+++ TaxVT.cpp
@@ -10,25 +10,50 @@ TaxVT::TaxVT(double income, filingStatus filing) {
 TaxVT::TaxVT() : TaxVT(0, single) {}
 
 // Filing singly
-double TaxVT::scheduleX() const {
+double TaxVT::scheduleX(double income) {
     if (income > 200200) {
-        return 12968 + ((income - 200200) * 8.75);
+        return 12968 + ((income - 200200) * 0.0875);
     } else if (income > 96000) {
-        return 5049 + ((income - 39600) * 7.60);
+        return 5049 + ((income - 39600) * .0760);
     } else if (income > 75000) {
-        return 3663 + ((income - 75000) * 6.60);
+        return 3663 + ((income - 75000) * 0.0660);
     } else if (income > 39600) {
-        return 1327 + ((income - 39600) * 6.60);
+        return 1327 + ((income - 39600) * 0.0660);
     } else {
-        return income * 3.35;
+        return income * 0.0335;
     }
 }
 
 // Filing separate, married
-double TaxVT::scheduleY1() const {}
+double TaxVT::scheduleY1(double income) {
+    if (income > 121875) {
+        return 7388 + ((income - 121875) * 0.0875);
+    } else if (income > 79975) {
+        return 4203 + ((income - 79975) * 0.0760);
+    } else if (income > 75000) {
+        return 3875 + ((income - 75000) * 0.0660);
+    } else if (income > 33075){
+        return 1108 + ((income - 33075) * 0.0660);
+    } else {
+        return income * 0.0335;
+    }
+}
 
 // Filing together, married
-double TaxVT::scheduleY2() const {}
+double TaxVT::scheduleY2(double income) {
+    return 0;
+}
 
 // Filing as head of household
-double TaxVT::scheduleZ() const {}
+double TaxVT::scheduleZ(double income) {
+    return 0;
+}
+
+// Get Taxes
+double TaxVT::getTaxes() {
+   if (filing == single) {
+       return scheduleX(income);
+   }
+   return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "TaxVT.cpp"
 #include <iostream>
+#include "TaxVT.h"
 
 using namespace std;
 
 int main() {
-    TaxVT griffin(10000);
+    TaxVT griffin(75000, single);
 
     cout << "2019 Taxes owed: " << griffin.getTaxes();
 
